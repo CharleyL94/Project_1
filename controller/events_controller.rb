089b52dcf2d80e3d9be6b62d6aa22ce586dc7396
Charley Lawton <user@CODECLAN055.local>
2016-08-14 18:18:19 +0100

    event first

--- controller/events_controller.rb
+++ controller/events_controller.rb
@@ -12,3 +12,19 @@ end
 get '/events/:id' do
   @event = Event.find(params["id"])
   erb :'events/show'
+end
+
+get '/events/:id/edit' do
+  end
+
+post '/events' do
+  @event = Event.new(params)
+  @event.save
+  redirect to("events")
+end
+
+post '/events/:id' do
+end
+
+delete '/events' do
+  end
