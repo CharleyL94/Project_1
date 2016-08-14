c71349f78d4b5804443dc7b6bfa06c9e78136f7f
Charley Lawton <user@CODECLAN055.local>
2016-08-14 20:09:05 +0100

    nations models

--- models/nation.rb
+++ models/nation.rb
@@ -3,4 +3,42 @@ require_relative('../db/sql_runner')
 
 class Nation
 
+  attr_reader :id, :name
+
+  def initialize( options )
+    @id = options['id'].to_i
+    @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO nations (name) VALUES ('#{@name}' ) RETURNING *"
+    member = SqlRunner.run(sql).first
+    @id = member['id']
+  end
+
+  def initialize( options )
+    @id = options['id'].to_i
+    @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO nations (name) VALUES ('#{@name}') RETURNING *"
+    nations = SqlRunner.run(sql).frist
+    @id = nation['id']
+  end
+
+  # def ?what to put here?()
+  #   sql = ?
+  #   return ?.map_items(sql)
+  # end
+
+  def self.all()
+    sql = "SELECT * FROM nations"
+    return Nation.map_items(sql)
+  end
+
+
+
+
+  
 end
