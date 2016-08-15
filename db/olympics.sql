dd256346096189c5f725317532176913d1c6d2d5
Charley Lawton <user@CODECLAN055.local>
2016-08-15 10:07:20 +0100

    Table Contents

--- db/olympics.sql
+++ db/olympics.sql
@@ -1,7 +1,12 @@
-DROP TABLE athletes;
+DROP TABLE participations;
 DROP TABLE events;
-DROP TABLE participation;
+DROP TABLE athletes;
 DROP TABLE nations;
+
+CREATE TABLE nations(
+  id serial4 PRIMARY KEY,
+  name VARCHAR(255)
+);
 
 CREATE TABLE athletes (
   id serial4 PRIMARY KEY,
@@ -9,23 +14,18 @@ CREATE TABLE athletes (
   nations_id int4 references nations(id) ON DELETE CASCADE
 );
 
-CREATE TABLE nations(
-  id serial4 PRIMARY KEY,
-  name VARCHAR(255)
-);
-
 CREATE TABLE events (
   id serial4 PRIMARY KEY,
   name VARCHAR(255),
-  gold_id references athletes(id) ON DELETE CASCADE,
-  silver_id references athletes(id) ON DELETE CASCADE,
-  brozne_id references athletes(id) ON DELETE CASCADE
+  gold_id int4 references athletes(id) ON DELETE CASCADE,
+  silver_id int4 references athletes(id) ON DELETE CASCADE,
+  brozne_id int4 references athletes(id) ON DELETE CASCADE
 );
 
 CREATE TABLE participations (
 id serial4 PRIMARY KEY,
 athlete_id int4 references athletes(id) ON DELETE CASCADE,
-event_id references events(id) ON DELETE CASCADE
+event_id int4 references events(id) ON DELETE CASCADE
 
 
 );
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,35 @@ Event.delete_all()
 Nation.delete_all()
 Participation.delete_all()
 
+athlete1 = Athlete.new({'name' => 'Usian Bolt', 'Nation' => 'Jamaica', 'event' => '100 metres'})
+athlete2 = Athlete.new({'name' => 'Justin Gatlin', 'Nation' => 'USA', 'event' => '100 metres'})
+athlete3 = Athlete.new({'name' => 'Andre De Grasse', 'Nation' => 'Canada', 'event' => '100 metres'})
+athlete4 = Athlete.new({'name' => 'Nafissatou Thaim', 'Nation' => 'Belguim', 'event' => 'Heptathlon'})
+athlete5 = Athlete.new({'name' => 'Jessica Ennis-Hill', 'Nation' => 'UK', 'event' => 'Heptathlon'})
+athlete6 = Athlete.new({'name' => 'Brianne Theisen Eaton', 'Nation' => 'Canada', 'event' => 'Heptathlon'})
+athlete7 = Athlete.new({'name' => 'Justin Rose', 'Nation' => 'GB', 'event' => 'golf'})
+athlete8 = Athlete.new({'name' => 'Henrik Stenson', 'Nation' => 'Sweden', 'event' => 'golf'})
+athlete9 = Athlete.new({'name' => 'Matt Kuchar', 'Nation' => 'USA', 'event' => 'golf'})
+athlete10 = Athlete.new({'name' => 'Max Whitlock', 'Nation' => 'GB', 'event' => 'Pommel Horse'})
+athlete11 = Athlete.new({'name' => 'Louis Smith', 'Nation' => 'GB', 'event' => 'Pommel Horse'})
+athlete12 = Athlete.new({'name' => 'Alexander Nddour', 'Nation' => 'USA', 'event' => 'Pommel Horse'})
+athlete13 = Athlete.new({'name' => 'Simone Biles', 'Nation' => 'USA', 'event' => 'All round Individual Gymanst'})
+athlete14 = Athlete.new({'name' => 'Alenandra Raisman', 'Nation' => 'USA', 'event' => 'All round Individual Gymanst'})
+athlete15 = Athlete.new({'name' => 'Ailya Mustafina', 'Nation' => 'Russia', 'event' => 'All round Individual Gymanst'})
+
+athlete1.save
+athlete2.save
+athlete3.save
+athlete4.save
+athlete5.save
+athlete6.save
+athlete7.save
+athlete8.save
+athlete9.save
+athlete10.save
+athlete11.save
+athlete12.save
+athlete13.save
+athlete14.save
+athlete15.save
 
